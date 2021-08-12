ab38fcc28957c5b4953dd14dd5dd4301b84cca96
Hudson Hancock <hudsonhancock@gmail.com>
2021-08-12 13:57:33 -0400

    Added dynamic features to recruitment video

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,13 +1,38 @@
-const video = document.getElementById("recruitVid");
+const video = document.querySelector("#recruitVid");
 const vidContainer = document.getElementById("vidContainer");
+var playBtn = document.querySelector("#playBtn");
+var pauseBtn = document.querySelector("#pauseBtn");
+var banner = document.querySelector("#linkBanner");
 
-video.addEventListener("ended", function () {
-  var buttonEl = document.createElement("a");
-  buttonEl.setAttribute("href", "#pfa");
-});
+// currentVideoTime = video.currentTime;
 
-if (video.currentTime > 20) {
-  alert("hello");
+function playVideo() {
+  video.play();
+  playBtn.classList.add("hide");
+  pauseBtn.classList.remove("hide");
 }
 
-/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeem-B8xCe-jEv49qcXm86hHNjxjZtV2v-ESobUgke8dFShAw/viewform?embedded=true" width="640" height="1548" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */
+function videoTime() {
+  console.log(video.currentTime);
+}
+
+setInterval(videoTime, 1000);
+
+video.addEventListener("timeupdate", function () {
+  if (video.currentTime >= 4) {
+    banner.classList.remove("hide");
+  }
+});
+
+function pauseVideo() {
+  video.pause();
+  pauseBtn.classList.add("hide");
+  playBtn.classList.remove("hide");
+}
+
+video.addEventListener("ended", function () {
+  pauseBtn.classList.add("hide");
+});
+
+playBtn.addEventListener("click", playVideo);
+pauseBtn.addEventListener("click", pauseVideo);
