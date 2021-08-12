f91e21c6a9a8fc6b621354fcfca9db5dbba3109a
Hudson Hancock <hudsonhancock@gmail.com>
2021-08-12 10:40:54 -0400

    added button banner

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -2,14 +2,12 @@ const video = document.getElementById("recruitVid");
 const vidContainer = document.getElementById("vidContainer");
 
 video.addEventListener("ended", function () {
-  var buttonEl = document.createElement('a');
-  buttonEl.setAttribute("href", "#pfa")
+  var buttonEl = document.createElement("a");
+  buttonEl.setAttribute("href", "#pfa");
 });
 
-if(video.currentTime > 20){
-  var sectionEl = document.createElement('section');
-  
+if (video.currentTime > 20) {
+  alert("hello");
 }
 
-
-/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeem-B8xCe-jEv49qcXm86hHNjxjZtV2v-ESobUgke8dFShAw/viewform?embedded=true" width="640" height="1548" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
+/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeem-B8xCe-jEv49qcXm86hHNjxjZtV2v-ESobUgke8dFShAw/viewform?embedded=true" width="640" height="1548" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */
